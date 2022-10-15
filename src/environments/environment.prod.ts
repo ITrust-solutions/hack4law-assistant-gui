2ad717b3500d6b7eab33b411d4a778750d760820
Kamil Baran <kamil.baran.it@gmail.com>
2022-10-15 20:54:40 +0200

    prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAkAIm6PcxrO_5Qv2bCv0kQWX1-zZ_-a0k",
+    authDomain: "fast-87830.firebaseapp.com",
+    projectId: "fast-87830",
+    storageBucket: "fast-87830.appspot.com",
+    messagingSenderId: "390718850993",
+    appId: "1:390718850993:web:ef011a4b2545aa470a1f90"
+  }
 };
